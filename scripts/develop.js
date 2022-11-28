28d2f34a6c7e3678a8f88933ca03e91727b7c3b3
Rémi de Juvigny <remi.dejuvigny@strapi.io>
2022-11-28 18:34:20 +0100

    More advanced prompts

--- scripts/develop.js
+++ scripts/develop.js
@@ -10,23 +10,51 @@ const questions = [
   {
     type: 'list',
     name: 'useCase',
-    message: 'What do you want to do?',
+    message: 'Which Strapi app do you want to run?',
     choices: [
       {
-        name: 'Point to an existing Strapi app',
-        value: 'point',
+        name: 'Get Started',
+        value: './examples/getstarted',
       },
       {
-        name: 'Create a new Strapi app',
-        value: 'create',
+        name: 'Kitchen Sink',
+        value: './examples/kitchensink',
+      },
+      {
+        name: 'Another existing app',
+        value: 'existing',
+      },
+      {
+        name: 'A new temporary app',
+        value: 'new',
       },
     ],
   },
   {
     type: 'input',
-    name: 'directory',
+    name: 'existingAppPath',
     message: 'What is the path to your Strapi app?',
-    when: (answers) => answers.useCase === 'point',
+    when: (answers) => answers.useCase === 'existing',
+  },
+  {
+    type: 'list',
+    name: 'existingAppPath',
+    message: 'Do you want to use a template?',
+    when: (answers) => answers.useCase === 'new',
+    choices: [
+      {
+        name: 'No, create an empty app',
+        value: null,
+      },
+      {
+        name: 'Yes, I want a blog',
+        value: 'blog',
+      },
+      {
+        name: 'Yes, I want a corporate site',
+        value: 'corporate',
+      },
+    ],
   },
 ];
 
@@ -36,15 +64,30 @@ program
   .action(runDevelop)
   .parse(process.argv);
 
-function runApp(path) {
-  return execa('yarn', ['develop'], {
-    stdio: 'inherit',
-    cwd: path,
-  });
+function getAppPath(answers) {
+  if (answers.useCase === 'new') {
+    // TODO: create a new app
+    throw new Error('Not implemented yet');
+    // Either use the create-strapi-app package via execa
+    // Or call @strapi/generate-new with some hardcoded options
+    // Maybe just let users pick a template?
+  }
+  if (answers.useCase === 'existing') {
+    return answers.existingAppPath;
+  }
+  return answers.useCase;
 }
 
 async function runDevelop() {
+  // Find info about the app to run
   const answers = await inquirer.prompt(questions);
-  console.log(answers);
-  runApp(answers.directory);
+  const appPath = getAppPath(answers);
+
+  // Run the app
+  // TODO: find why build not updating
+  execa('yarn', ['develop'], {
+    stdio: 'inherit',
+    cwd: appPath,
+  });
+  // TODO: handle process exit to delete the app if it was temporary
 }
