c55852eb59557e84f4352cb9d5252c7d8a2333b1
fdel-car <fabien.delcarmen@gmail.com>
2022-12-12 20:51:47 +0100

    Add cron job to dynamically update the license

--- packages/core/strapi/ee/index.js
+++ packages/core/strapi/ee/index.js
@@ -7,6 +7,7 @@ const fetch = require('node-fetch');
 const { pick } = require('lodash/fp');
 
 const { coreStoreModel } = require('../lib/services/core-store');
+const { getRecurringCronExpression } = require('../lib/utils/cron');
 
 const publicKey = fs.readFileSync(join(__dirname, 'resources/key.pub'));
 
@@ -34,30 +35,33 @@ const readLicense = (directory) => {
 };
 
 const fetchLicense = async (key, fallback) => {
+  const fallbackToStoredLicense = () => {
+    const error = 'Could not proceed to the online verification of your license.';
+
+    if (fallback) {
+      ee.logger(`${error} We will try to use the locally stored one as a potential fallback.`);
+      return fallback;
+    }
+
+    disable(`${error} Sorry for the inconvenience. Starting in CE.`);
+    return null;
+  };
+
   try {
     const response = await fetch(`https://license.strapi.io/api/licenses/${key}`);
 
-    if (response.status !== 200) {
+    if (response.status >= 500) {
+      return fallbackToStoredLicense();
+    }
+
+    if (response.status >= 400) {
       disable();
       return null;
     }
 
     return response.text();
-  } catch (error) {
-    if (error instanceof fetch.FetchError) {
-      if (fallback) {
-        ee.logger(
-          'Could not proceed to the online verification of your license. We will try to use your locally stored one as a potential fallback.'
-        );
-        return fallback;
-      }
-
-      disable(
-        'Could not proceed to the online verification of your license, sorry for the inconvenience. Starting in CE.'
-      );
-    }
-
-    return null;
+  } catch {
+    return fallbackToStoredLicense();
   }
 };
 
@@ -152,9 +156,8 @@ const onlineUpdate = async (db) => {
 
     await transaction.commit();
   } catch (error) {
-    // TODO: The database can be locked at the time of writing, could just a SQLite issue only
+    // The database can be locked at the time of writing, seems to just be a SQLite issue
     await transaction.rollback();
-    return disable(error.message);
   }
 };
 
@@ -165,7 +168,7 @@ const defaultFeatures = {
 };
 
 const validateInfo = () => {
-  if (ee.licenseInfo.expireAt) {
+  if (!ee.licenseInfo.expireAt) {
     return;
   }
 
@@ -182,30 +185,34 @@ const validateInfo = () => {
   }
 };
 
+const recurringCheck = async ({ strapi }) => {
+  await onlineUpdate(strapi.db);
+  validateInfo();
+};
+
+// This env variable support is temporary to ease the migration between online vs offline
 const shouldStayOffline = process.env.STRAPI_DISABLE_LICENSE_PING?.toLowerCase() === 'true';
 
-const checkLicense = async (db) => {
+const checkLicense = async ({ strapi }) => {
   if (!shouldStayOffline) {
-    await onlineUpdate(db);
-    // TODO: Register cron, try to spread it out across projects to avoid regular request spikes
+    await onlineUpdate(strapi.db);
+    const now = new Date();
+    strapi.cron.add({ [getRecurringCronExpression(now)]: recurringCheck });
   } else if (!ee.licenseInfo.expireAt) {
     return disable('Your license does not have offline support. Starting in CE.');
   }
 
-  if (ee.enabled) {
-    validateInfo();
-  }
+  validateInfo();
 };
 
 module.exports = {
   init,
-  disable,
-  features: {
-    isEnabled: (feature) => (ee.enabled && ee.licenseInfo.features?.includes(feature)) || false,
-    getEnabled: () => (ee.enabled && Object.freeze(ee.licenseInfo.features)) || [],
-  },
   checkLicense,
   get isEE() {
     return ee.enabled;
+  },
+  features: {
+    isEnabled: (feature) => (ee.enabled && ee.licenseInfo.features?.includes(feature)) || false,
+    getEnabled: () => (ee.enabled && Object.freeze(ee.licenseInfo.features)) || [],
   },
 };
